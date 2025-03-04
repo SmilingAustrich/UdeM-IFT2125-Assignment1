168c062653b57d38176ee81cf522edd8e5276499
SmilingAustrich <144949380+SmilingAustrich@users.noreply.github.com>
2025-03-04 15:03:05 -0500

    Base files + report

---
+++ Q4/MaxToysCalculator.cpp
@@ -0,0 +1,17 @@
+#include "MaxToysCalculator.h"
+#include <vector>
+#include <math.h>
+#include <iostream>
+
+// ce fichier contient les definitions des methodes de la classe MaxToysCalculator
+// this file contains the definitions of the methods of the MaxToysCalculator class
+
+using namespace std;
+
+MaxToysCalculator::MaxToysCalculator()
+{
+}
+
+int MaxToysCalculator::CalculateMaxToys(const vector<int>& Toys, int S) {
+    // Compléter ici  
+}
